1cb49b56e84bfd5aef7b92d4a9a1fb408f584c7c
lucasrodrigues117 <rodrigues.lds.lfr@gmail.com>
2025-12-02 22:02:20 -0300

    mudança nos arquivos principais

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-const stacks = document.querySelectorAll(".stack");
+// STACK 1
+function salvarStack1() {
+    const dados = {
+        subida: true,
+        rpm_medio: document.getElementById("valorA").value,
+        peso_veiculo: document.getElementById("valorB").value,
+        tipo_terreno: "stack1"
+    };
 
-stacks.forEach(stack => {
-    stack.addEventListener("mouseenter", () => {
-        stacks.forEach(s => s.classList.remove("active")); // fecha todas
-        stack.classList.add("active"); // abre somente a ativa
-    });
-});
-    
+    enviarDados(dados);
+}
+
+// STACK 2
+function salvarStack2() {
+    const dados = {
+        subida: false,
+        rpm_medio: document.getElementById("temperatura").value,
+        peso_veiculo: document.getElementById("pressao").value,
+        tipo_terreno: "stack2"
+    };
+
+    enviarDados(dados);
+}
+
+// STACK 3
+function salvarStack3() {
+    const dados = {
+        subida: false,
+        rpm_medio: document.getElementById("frequencia").value,
+        peso_veiculo: document.getElementById("amplitude").value,
+        tipo_terreno: "stack3"
+    };
+
+    enviarDados(dados);
+}
+
+// FUNÇÃO QUE ENVIA PARA O BACKEND
+function enviarDados(dados) {
+    fetch("http://127.0.0.1:8000/api/variaveis/", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Enviado:", data);
+        alert("Dados enviados!");
+    })
+    .catch(error => console.error("Erro:", error));
+}
